2117e2390503eca562a17d57b4d83543c4756e75
decavacado <60301669+decavacado@users.noreply.github.com>
2021-03-03 09:23:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const discord = require("discord.js");
+const client = new discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+function parser(msg) {
+  let arr = msg.split(" ")
+  return {
+    command: arr.shift(),
+    args: arr
   }
-});
+}
+
+client.on("message", function(msg) {
+  let command = parser(msg.content)
+  if(msg.content === "c!ping") {
+    msg.reply("Pong!")
+  }else if(command.command === "c!add") {
+    let total = 0
+    
+
+    for(let i of command.args) {
+      total += parseInt(i)
+    }
+
+    msg.reply(total);
+  }else if(command.command === "c!sub") {
+    let total = parseInt(command.args[0]);
+    command.args.shift();
+    for(let i of command.args) {
+      total -= parseInt(i)
+    }
+
+    msg.reply(total);
+  }else if(command.command === "c!mul") {
+    let total = parseInt(command.args[0]);
+    command.args.shift();
+    for(let i of command.args) {
+      total *= parseInt(i)
+    }
+
+    msg.reply(total);
+  }else if(command.command === "c!space") {
+        //later
+    })
+  }
+  console.log(command)
+})
+
 
 client.login('token');
